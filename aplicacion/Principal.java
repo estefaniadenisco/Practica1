5e29ee5caa7fb3669d08f7005029d75ac34d5ed2
Estefania <tefidenisco@gmail.com>
2023-02-12 12:19:24 +0100

    completo

---
+++ aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package aplicacion;
+import mates.Matematicas;
+
+public class Principal{
+	public static void main(String[]args){
+		system.out.println("El numero PI es "+Matematicas.generarNumeroPi(Integer.parseInt(args[0])));
+
+	}
+}
+
